6a6a05a45dc9f1d0a2ba79e910b60413bc7904b8
Aislinn00 <eizizawarkhin04@gmail.com>
2025-12-14 04:35:22 +0000

    fix errors

--- backend/app.py
+++ backend/app.py
@@ -20,15 +20,16 @@ def test_db():
     return jsonify({"database": db[0]})
 
 # IMPORT ROUTES AFTER app exists
-from backend.routes.auth_routes import auth_routes
-from backend.routes.vehicle_routes import vehicle_routes
-from backend.routes.maintenance_routes import maintenance_routes
-from backend.routes.reminder_routes import reminder_routes
-#from backend.routes.vehicle_image_routes import vehicle_image_routes
+from routes.auth_routes import auth_routes
+from routes.vehicle_routes import vehicle_routes
+from routes.maintenance_routes import maintenance_routes
+from routes.reminder_routes import reminder_routes
+from routes.vehicle_image_routes import vehicle_image_routes
+
 
 app.register_blueprint(auth_routes)
 app.register_blueprint(vehicle_routes)
 app.register_blueprint(maintenance_routes)
 app.register_blueprint(reminder_routes)
-# app.register_blueprint(vehicle_image_routes)
+app.register_blueprint(vehicle_image_routes) 
 
