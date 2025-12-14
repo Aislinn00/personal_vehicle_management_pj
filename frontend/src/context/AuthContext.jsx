389d6f0de8fd5f5fddca0880a385761baf6a645e
Aislinn00 <eizizawarkhin04@gmail.com>
2025-12-14 10:07:41 +0000

    Implement the codes of stored user

--- frontend/src/context/AuthContext.jsx
+++ frontend/src/context/AuthContext.jsx
@@ -4,19 +4,26 @@ const AuthContext = createContext();
 
 export const AuthProvider = ({ children }) => {
   const [token, setToken] = useState(localStorage.getItem("token"));
+  const [user, setUser] = useState(
+    JSON.parse(localStorage.getItem("user"))
+  );
 
-  const login = (jwt) => {
+  const login = (jwt, userData) => {
     localStorage.setItem("token", jwt);
+    localStorage.setItem("user", JSON.stringify(userData));
     setToken(jwt);
+    setUser(userData);
   };
 
   const logout = () => {
     localStorage.removeItem("token");
+    localStorage.removeItem("user");
     setToken(null);
+    setUser(null);
   };
 
   return (
-    <AuthContext.Provider value={{ token, login, logout }}>
+    <AuthContext.Provider value={{ token, user, login, logout }}>
       {children}
     </AuthContext.Provider>
   );
