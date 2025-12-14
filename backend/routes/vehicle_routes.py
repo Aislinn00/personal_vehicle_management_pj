0cff2463ae0bf3a0ec25bdefc9d6c2cdf85b2c39
Aislinn00 <eizizawarkhin04@gmail.com>
2025-12-14 04:37:39 +0000

    fix errors

--- backend/routes/vehicle_routes.py
+++ backend/routes/vehicle_routes.py
@@ -1,7 +1,7 @@
 from flask import Blueprint, request, jsonify, g
 from mysql.connector import Error as MySQLError
-from backend.db import get_connection
-from backend.utils.auth_role import auth_required
+from db import get_connection
+from utils.auth_role import auth_required
 
 vehicle_routes = Blueprint("vehicle_routes", __name__)
 
